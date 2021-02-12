ba6c23ce747021d5c7d902a971aa4c42e8c6a4f4
marijagraovac <73234483+marijagraovac@users.noreply.github.com>
2021-02-12 21:15:54 +0100

    Delete util.py

--- util.py
+++
@@ -1,8 +0,0 @@
-def bubble_sort(lista, kljuc):
-    n = len(lista)
-    for j in range(n):
-        for i in range(n-1-j):
-            if lista[i][kljuc] > lista[i+1][kljuc]:
-                temp = lista[i]
-                lista[i] = lista[i+1]
-                lista[i+1] = temp
